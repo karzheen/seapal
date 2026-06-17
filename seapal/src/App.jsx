2976321d1f102e87aed39c7ee6ae9a561b695776
karzheen <159645566+karzheen@users.noreply.github.com>
2026-06-17 11:43:55 +0300

    only side rec left

--- seapal/src/App.jsx
+++ seapal/src/App.jsx
@@ -4,8 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Gallery from "./pages/gallery";
 import DetailCard from "./component/detailCard";
-
+/* FIX: Changed path structure from absolute "src/img/..." to relative "./img/..." */
+import siderec from "./img/siderec.svg";
 import Footer from "./component/footer";
+
 function Layout({ children }) {
   return (
     <div className="layout">
@@ -28,7 +30,6 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/detail/:id" element={<DetailCard />} />
-       
       </Routes>
     </Layout>
   );
