0975fa107678e111d21e1597ffde61382be11fc0
karzheen <159645566+karzheen@users.noreply.github.com>
2026-06-21 12:27:49 +0300

    navbar3

--- seapal/src/pages/navbar.jsx
+++ seapal/src/pages/navbar.jsx
@@ -9,9 +9,9 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <section className="logo">
-        {/* Fixed: Added /img/ prefix to resolve the branding vectors correctly */}
-        <img src="/img/logo.svg" alt="logo" />
-        <img src="/img/name.svg" alt="brand name" />
+        {/* Fixed: Injected Vite base URL variable to dynamically match GitHub subfolder paths */}
+        <img src={`${import.meta.env.BASE_URL}img/logo.svg`} alt="logo" />
+        <img src={`${import.meta.env.BASE_URL}img/name.svg`} alt="brand name" />
       </section>
 
       <div className="links">
@@ -25,12 +25,12 @@ export default function Navbar() {
           About
         </NavLink>
         
-        {/* FIX: Use standard anchor elements for external social media routes */}
+        {/* FIX: Injected Vite base URL variable to dynamically fix social media icon 404s */}
         <a href="https://instagram.com" target="_blank" rel="noreferrer" className="social-icon-link">
-          <img src="img/instagram.svg" alt="instagram icon" />  
+          <img src={`${import.meta.env.BASE_URL}img/instagram.svg`} alt="instagram icon" />  
         </a>
         <a href="https://facebook.com" target="_blank" rel="noreferrer" className="social-icon-link">
-          <img src="img/facebook.svg" alt="facebook icon" />
+          <img src={`${import.meta.env.BASE_URL}img/facebook.svg`} alt="facebook icon" />
         </a>
       </div>
     </nav>
