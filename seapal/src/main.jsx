17a409a0f3346add1937d43bbd3bc98fe36a490b
karzheen <159645566+karzheen@users.noreply.github.com>
2026-08-11 15:59:55 +0300

    Update main.jsx

--- seapal/src/main.jsx
+++ seapal/src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* Explicitly tell React Router it is running inside the /seapal subfolder */}
+    <BrowserRouter basename="/seapal">
       <App />
     </BrowserRouter>
   </StrictMode>
